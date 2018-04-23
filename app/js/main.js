4be6e036c1d598cf1f9d9100a71d3800a803fa71
milos.radoman <milos.radoman@puzzlesoftware.rs>
2018-04-23 18:46:41 +0200

    Frst commit

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,89 @@
-/** @description Main function where wee initialize our application 
- * @function toastr is used to log in a nice way insted of using console log* **/
+/** @author  Radoman Milos
+ * @description Prize game for matchabout.com
+ *
+ */
 
-(function(global, toastr, $, application) {
-    'use strict';
+(function(global, toastr, $) {
+	'use strict';
+	var socket = io('https://www.matchabout.com:443');
+	var data = [];
+	var spin = true;
+	var rouletter = $('div.roulette');
+	var roulete = $('.roulette');
+	var profile = $('.profile');
+	var option = {
+		speed: 5,
+		duration: 3,
+		stopImageNumber: 4,
+		startCallback: function() {
+			console.log('start');
+		},
+		slowDownCallback: function() {
+			console.log('slowDown');
+		},
+		stopCallback: function($stopElm) {
+			roulete.fadeOut();
+			profile.show();
+		},
+	};
 
-    $(".add").click(function() {
-        var value = $('#city').val();
-        application.getCity(value);
-    });
+	var createImage = function(image) {
+		if (!image) {
+			roulete.append('<img style="height:200px" src="http://via.placeholder.com/200x200">');
+		} else {
+			roulete.append('<img style="height:200px" src="data:image/png;base64,' + image + ' ">');
+		}
+	};
 
-    $('.weathercards').on('click', '.weathercards__card__close', function() {
-        $(this).closest(".weathercards__card").hide();
-    })
+	var callSpin = function(data) {
+		spin = false;
+		console.log(data.length);
 
-    toastr.info('Loaded App!');
-})(window, toastr, jQuery, app);
+		for (var index = 0; index < data.length; index++) {
+			var image = _arrayBufferToBase64(data[index].picture);
+			createImage(image);
+			if (index == 4) {
+				$('.photo img').attr('src', 'data:image/png;base64,' + image + '');
+				$('.text h3').html(data[index].firstName + ' ' + data[index].lastName);
+				$('.text h6').html(data[index].title);
+			}
+		}
+
+		rouletter.roulette(option);
+		rouletter.roulette('start');
+	};
+
+	$('.spin').click(function() {
+		rouletter.roulette('start');
+		socket.emit('start');
+	});
+	socket.on('conection', function() {
+		console.log('Consected');
+	});
+	socket.on('prizeGame', function(data) {
+		if (spin) {
+			console.log(data);
+			spin = false;
+			callSpin(data);
+		}
+	});
+	socket.on('message', function(data) {
+		console.dir(data);
+	});
+	socket.on('event', function(data) {
+		console.dir(data);
+	});
+
+	/** @function _arrayBufferToBase64 turns binary arrays that we recive from backend into image  */
+
+	function _arrayBufferToBase64(buffer) {
+		var binary = '';
+		var bytes = new Uint8Array(buffer);
+		var len = bytes.byteLength;
+		for (var i = 0; i < len; i++) {
+			binary += String.fromCharCode(bytes[i]);
+		}
+		return window.btoa(binary);
+	}
+	$('.show').click(function() {});
+})(window, toastr, jQuery);
--- index.js
+++ index.js
@@ -13,15 +13,11 @@ var bodyParser = require('body-parser');
 var logger = require('morgan');
 var fs = require('fs');
 var http = require('http');
-var nodemailer = require("nodemailer");
-
-
-
 
 /* app uses port 5000 or environments port, in our case 
    our production server will be on Heroku, but local server is on 5000
  */
-app.set('port', (process.env.PORT || 5000));
+app.set('port', process.env.PORT || 5000);
 app.use(express.static(__dirname + '/app'));
 
 app.use(bodyParser.json());
@@ -30,15 +26,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(logger('dev'));
 app.set('app', __dirname + '/app');
 
-
 app.get('/', function(request, response) {
-    response.render('index');
+	response.render('index');
 });
 
-
-
-
-
 app.listen(app.get('port'), function() {
-    console.log('Node app is running on', app.get('port'));
+	console.log('Node app is running on', app.get('port'));
 });
